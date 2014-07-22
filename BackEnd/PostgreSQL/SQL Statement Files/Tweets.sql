19c1ff8245faaf5653239d46897e7571f2a031e0
Nadine <Nadine_Engelhardt@web.de>
2014-07-22 11:15:30 -0400

    database dump of twitterdata, changed sql statements

--- BackEnd/PostgreSQL/SQL Statement Files/Tweets.sql
+++ BackEnd/PostgreSQL/SQL Statement Files/Tweets.sql
@@ -4,10 +4,13 @@
    keywordid int,
    dateid int,
    latitude double precision,
-   longitude double precision,
-   content character varying(140)
+   longtude double precision,
+   content character varying(140),
+   cityid int
 ) 
 ;
 
 CREATE SEQUENCE tweet_id_seq;
 ALTER TABLE "twitterdata" ALTER "tweet_id" SET DEFAULT NEXTVAL('tweet_id_seq');
+
+select * from "twitterdata"
