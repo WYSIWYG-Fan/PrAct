5fbc2e122ae8ff668b967e5e46a080958895ad2a
Nadine <Nadine_Engelhardt@web.de>
2014-07-22 11:53:40 -0400

    Implemented php database connection working with XAMPP

--- FrontEnd/Source Code/myAjax.php
+++ FrontEnd/Source Code/myAjax.php
@@ -1,4 +1,26 @@
-// file myAjax.php
 <?php
-  echo 'hello world!';
-?>
+// Verbindungsaufbau und Auswahl der Datenbank
+$dbconn = pg_connect("host=localhost dbname=PrAct user=postgres password=password")
+    or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
+
+// Eine SQL-Abfrge ausführen
+$query = 'SELECT * FROM citydimension';		//just for testing
+$result = pg_query($query) or die('Abfrage fehlgeschlagen: ' . pg_last_error());
+
+// Ergebnisse in HTML ausgeben
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Speicher freigeben
+pg_free_result($result);
+
+// Verbindung schließen
+pg_close($dbconn);
+?> 
