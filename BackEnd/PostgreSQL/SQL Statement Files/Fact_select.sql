ffd28aa5f215018f6b09141850386313a1965c0b
Patrick H <dalamardunkelelf@googlemail.com>
2014-07-22 23:12:20 +0200

    facts data and dump

--- BackEnd/PostgreSQL/SQL Statement Files/Fact_select.sql
+++ BackEnd/PostgreSQL/SQL Statement Files/Fact_select.sql
@@ -14,7 +14,9 @@ and 	t.keywordid = keyword_id
 group by k.keyword_id, k.german, dd.date_id
 order by dd.date_id, count(k.keyword_id) desc
 
+--fact insert
 
+insert into facts (activity_id, weather_id, season_id, ranking)
 select 	k.keyword_id, 
 	wd.weather_id,
 	dd.seasonid,
@@ -27,6 +29,18 @@ from 	tweetkeywords k,
 where	wd.weather_id = w.weatherdimension
 and	w.date = dd.date_id
 and	dd.date_id = t.dateid
-and 	t.keywordid = keyword_id
+and 	t.keywordid = k.keyword_id
+and 	t.dateid = w.date
+and 	t.cityid = w.location
 group by k.keyword_id, wd.weather_id, dd.seasonid
 order by wd.weather_id, count(k.keyword_id) desc
+
+select t.keywordid, w.weather_id, count(*) from twitterdata t, -- where keywordid = 7;
+weatherdata2, weatherdimension w where t.dateid = weatherdata2.date
+and w.weather_id = weatherdata2.weatherdimension
+group by t.keywordid, w.weather_id
+order by count(*)
+
+select count(*) from twitterdata where keywordid = 20
+
+select w.weather_id from weatherdimension w
