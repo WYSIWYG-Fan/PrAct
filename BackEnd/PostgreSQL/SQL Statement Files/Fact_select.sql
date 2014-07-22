669b80a790d513f8c2aa538e96a0c1ae63b449cc
Patrick H <dalamardunkelelf@googlemail.com>
2014-07-22 20:37:33 +0200

    fact select prototype

---
+++ BackEnd/PostgreSQL/SQL Statement Files/Fact_select.sql
@@ -0,0 +1,32 @@
+﻿select 	k.keyword_id, 
+	k.german,
+	dd.date_id,
+	count(k.keyword_id)
+from 	tweetkeywords k, 
+	twitterdata t, 
+	datedimension dd, 
+	weatherdimension wd, 
+	weatherdata2 w
+where	wd.weather_id = w.weatherdimension
+and	w.date = dd.date_id
+and	dd.date_id = t.dateid
+and 	t.keywordid = keyword_id
+group by k.keyword_id, k.german, dd.date_id
+order by dd.date_id, count(k.keyword_id) desc
+
+
+select 	k.keyword_id, 
+	wd.weather_id,
+	dd.seasonid,
+	count(k.keyword_id)
+from 	tweetkeywords k, 
+	twitterdata t, 
+	datedimension dd, 
+	weatherdimension wd, 
+	weatherdata2 w
+where	wd.weather_id = w.weatherdimension
+and	w.date = dd.date_id
+and	dd.date_id = t.dateid
+and 	t.keywordid = keyword_id
+group by k.keyword_id, wd.weather_id, dd.seasonid
+order by wd.weather_id, count(k.keyword_id) desc
