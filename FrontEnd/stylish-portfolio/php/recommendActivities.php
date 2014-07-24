3e8afccf5b11a3282c88759a1a08bdea672c3c57
Nadine <Nadine_Engelhardt@web.de>
2014-07-23 22:35:53 -0400

    cleanded recommendActivities.php

--- FrontEnd/stylish-portfolio/php/recommendActivities.php
+++ FrontEnd/stylish-portfolio/php/recommendActivities.php
@@ -5,19 +5,6 @@ $dbconn = pg_connect("host=localhost dbname=PrAct user=postgres password=passwor
     or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
 
 // Query database
-/*$query = "select distinct k.german 
-from facts f 
-join weatherdimension w on f.weather_id = w.weather_id, 
-tweetkeywords k, 
-datedimension d join seasondimension s on s.season_id = d.seasonid 
-where ".$_GET("temp")." between w.min_temperature and w.max_temperature 
-and w.wind = ".$_GET("wind")."
-and precipitation =".$_GET("precip")." 
-and sunshine = ".$_GET("sun")." 
-and k.keyword_id = f.activity_id 
-and d.month = ".$_GET("month")." 
-and d.seasonid = f.season_id 
-order by ranking desc limit 10;"; */
 
 $query = "select distinct k.english, ranking
 from facts f 
@@ -41,7 +28,6 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
         echo "$col_value ";
     }
 }
-
 
 // free memory
 pg_free_result($result);
